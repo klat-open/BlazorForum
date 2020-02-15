3ac61468d8b9021c74f01bc6d3afdf811e087f0d
Steve Elliott <steve@elliottbrand.com>
2020-02-15 01:04:51 -0500

    Adjustments to the Create a New Topic functionality (mostly for the editor).

---
+++ BlazorForum.Domain/Utilities/Formatting/RegularExp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace BlazorForum.Domain.Utilities.Formatting
+{
+    public class RegularExp
+    {
+        public static string RemoveScriptTags(string valueToClean)
+        {
+            var pattern = @"<script[^>]*>[\s\S]*?</script>";
+            return Regex.Replace(valueToClean, pattern, "");
+        }
+    }
+}
